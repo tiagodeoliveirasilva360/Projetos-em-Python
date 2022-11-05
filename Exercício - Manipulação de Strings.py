19c1a659eb61044754e2f465bba5ec9e18d989f4
tiagodeoliveirasilva360 <115423894+tiagodeoliveirasilva360@users.noreply.github.com>
2022-11-05 15:56:15 -0300

    Create Exercício - Manipulação de Strings.py

---
+++ Exercício - Manipulação de Strings.py
@@ -0,0 +1,11 @@
+t1 = str(input("Digite uma palavra: "))
+print(t1)
+print(len(t1))
+
+t2 = str(input("digite outra palavra: "))
+print(t2)
+print(t2.upper())
+
+t3 = str(input("digite uma palavra maiuscula: "))
+print(t3)
+print(t3.lower())
